149753da13f761d2e20b58c35e338dc796e32a8f
Alisson Freire <alisson.batista@docente.senai.br>
2026-07-14 15:16:15 -0300

    Alterado as funções

--- js/carrinho.js
+++ js/carrinho.js
@@ -2,88 +2,20 @@
 let itensCarrinho = JSON.parse(sessionStorage.getItem("carrinhoSessao")) || []
 //const itensCarrinho2 = JSON.parse(sessionStorage.get('carrinhoSessao')) || []
 
-
 //FUNÇÃO PARA ADCIONAR UM ITEM
 const addItem = (objItem) => {
     itensCarrinho.push(objItem)
 
     sessionStorage.setItem('carrinhoSessao', JSON.stringify(itensCarrinho))
-
-    listItens()
 }
 
 //FUNÇÃO PARA LISTAR OS ITENS DO CARRINHO
 const listItens = () => {
     const listaItens =  JSON.parse(sessionStorage.getItem('carrinhoSessao')) 
 
-    montaTelaCarrinho(listItens)
-
+    return listaItens
 }
-
-//MONTAR TELA CARRINHO
-const montaTelaCarrinho = (objListaItens)=>{
-    const sectionItensCarrinho = document.querySelector('#itens-carrinho')
-    
-    objListaItens.forEach((elem, i) => {
-        const sectionItem = document.createElement('section')
-        sectionItem.setAttribute('class', item)
-
-        const divImgItem = document.createElement('div')
-        divImgItem.setAttribute('class', 'img-item')
-
-        const imgItem = document.createElement('img')
-        imgItem.setAttribute('src', elem.caminho_imagem)
-        imgItem.setAttribute('alt', elem.descricao_produto)
-
-        imgItem.appendChild(imgItem)
-
-        const divDescricaoItens = document.createElement('div')
-        divDescricaoItens.setAttribute('class', 'descricoes-itens')
-
-        const divDescricao = document.createElement('div')
-        divDescricao.setAttribute('class', 'descricao')
-        divDescricao.innerHTML = elem.descricao_produto
-
-        const divValores = document.createElement('div')
-        divValores.setAttribute('class', 'valores')
-
-        const pItem = document.createElement('p')
-        pItem.innerHTML = `R$ ${parseFloat(elem. valor_unitario).toFixed(2).replace('.',',')}`
-
-        const divQuant = document.createElement('div')
-        divQuant.setAttribute('class', input-quantidade)
-
-        const inputQuantidade = document.createElement('input')
-        inputQuantidade.setAttribute('type', 'number')
-        inputQuantidade.setAttribute('name', `quant${i}`)
-        inputQuantidade.setAttribute('id', `quant${i}`)
-        inputQuantidade.setAttribute('class', 'input-item')
-        inputQuantidade.setAttribute('value', 1)
-
-        divQuant.appendChild(inputQuantidade)
-
-        const pCalc = document.createElement('p')
-        pCalc.innerHTML = `R$ ${elem.valor_unitario * 1}`
-
-        const imgRemover = document.createElement('img')    
-        imgRemover.setAttribute('src', '../imagens/icones/remover.png')
-        imgRemover.setAttribute('alt', 'remover')
-
-        divValores.appendChild(pItem)
-        divValores.appendChild(divQuant)
-        divValores.appendChild(pCalc)
-        divValores.appendChild(imgRemover)
-        
-
-            
-    
-    })
-    
-}
-
-
-
 
 
 //EXPORTAÇÃO
-export { addItem }
+export { addItem, listItens }
