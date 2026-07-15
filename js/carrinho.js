9b1500018f79e9c5d6f9460bcd6a37172371eb26
Alisson Freire <alisson.batista@docente.senai.br>
2026-07-15 16:43:20 -0300

    Acrescido exemplo com localStorege

--- js/carrinho.js
+++ js/carrinho.js
@@ -3,9 +3,27 @@ let itensCarrinho = JSON.parse(sessionStorage.getItem("carrinhoSessao")) || []
 
 //const itensCarrinho = JSON.parse(localStorage.get('carrinhoSessao')) || []
 
+//FUNÇÃO ITEM
+const item = (objProduto) => {
+
+    const item = {
+        id_produto: objProduto.id_produto,
+        descricao_produto: objProduto.descricao_produto,
+        valor_unitario: objProduto.valor_unitario,
+        unidade: objProduto.unidade,
+        caminho_imagem: objProduto.caminho_imagem,
+        quantidade : 1,
+    }
+
+    return item
+
+}
+
+console.log(itensCarrinho.findIndex(elem => elem.id_produto == 80))
+
 //FUNÇÃO PARA ADCIONAR UM ITEM
 const addItem = (objItem) => {
-    itensCarrinho.push(objItem)
+    itensCarrinho.push(item(objItem))
 
     sessionStorage.setItem('carrinhoSessao', JSON.stringify(itensCarrinho))
     //localStorage.setItem('carrinhoSessao', JSON.stringify(itensCarrinho))
@@ -13,7 +31,7 @@ const addItem = (objItem) => {
 
 //FUNÇÃO PARA LISTAR OS ITENS DO CARRINHO
 const listItens = () => {
-    const listaItens =  JSON.parse(sessionStorage.getItem('carrinhoSessao')) 
+    const listaItens = JSON.parse(sessionStorage.getItem('carrinhoSessao'))
 
     //const itensCarrinho2 = JSON.parse(localStorage.get('carrinhoSessao')) || []
 
@@ -22,11 +40,11 @@ const listItens = () => {
 }
 
 //FUNÇÃO REMOVER ITEM DO ARRAY
-const removeItem = (pos)=>{
+const removeItem = (pos) => {
     itensCarrinho.splice(pos, 1)
 
     sessionStorage.setItem('carrinhoSessao', JSON.stringify(itensCarrinho))
-    
+
     //localStorage.setItem('carrinhoSessao', JSON.stringify(itensCarrinho))
 }
 
